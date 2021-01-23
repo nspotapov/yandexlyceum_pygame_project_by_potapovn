d60cd558c17ea38861887992b3b5cec960342db3
Nikita <nikitoski4@gmail.com>
2021-01-23 23:14:42 +0400

    Коммит первого дня

--- PacMan/__init__.py
+++ PacMan/__init__.py
@@ -2,10 +2,12 @@ from .game_control import GameControl
 from .main_menu import MainMenu
 from .button import Button
 from .program_states import ProgramState
+from .board import Board
 
 __all__ = [
     "GameControl",
     "MainMenu",
     "Button",
-    "ProgramState"
+    "ProgramState",
+    "Board"
 ]
