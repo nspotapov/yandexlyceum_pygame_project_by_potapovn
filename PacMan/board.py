b91bc04c231994f43946000ba23f4d8f9a6d79bb
Nikita <nikitoski4@gmail.com>
2021-01-26 10:33:22 +0400

    Заполненая карта и начата работа с классом карты

--- PacMan/board.py
+++ PacMan/board.py
@@ -1,2 +1,19 @@
+from settings import GAME_BOARD
+
+
 class Board:
-    pass
+    def __init__(self):
+        self.current_board_state = GAME_BOARD.copy()
+
+    def get_tile_id(self, row, col):
+        """Вохвращает id тайла на карте с координатами [row][col]"""
+        return self.current_board_state[row][col]
+
+    def get_current_board_state(self):
+        """Возвращает текущее состояние карты"""
+        return self.current_board_state.copy()
+
+    def set_tile_id(self, row, col):
+        """Устанавливает id тайла на карту с координатами [row][col]"""
+        # TODO
+        pass
