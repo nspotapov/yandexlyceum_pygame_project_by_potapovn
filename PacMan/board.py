8b9786711eb9d4f069ae5197b15f4b241de069e1
Nikita <nikitoski4@gmail.com>
2021-01-26 11:22:34 +0400

    Промежуточный коммит

--- PacMan/board.py
+++ PacMan/board.py
@@ -2,6 +2,8 @@ from settings import GAME_BOARD
 
 
 class Board:
+    """Класс клеточного поля игры"""
+
     def __init__(self):
         self.current_board_state = GAME_BOARD.copy()
 
@@ -9,11 +11,10 @@ class Board:
         """Вохвращает id тайла на карте с координатами [row][col]"""
         return self.current_board_state[row][col]
 
+    def set_tile_id(self, row, col, tile_id):
+        """Устанавливает id тайла на карту с координатами [row][col]"""
+        self.current_board_state[row][col] = tile_id
+
     def get_current_board_state(self):
         """Возвращает текущее состояние карты"""
         return self.current_board_state.copy()
-
-    def set_tile_id(self, row, col):
-        """Устанавливает id тайла на карту с координатами [row][col]"""
-        # TODO
-        pass
